4f5933161a4673529ad736ce20135be1bf66a0cd
Franciszek Czana <franciszek.czana@futurum.tech>
2020-04-27 20:06:09 +0200

    implement authentication by users set in database

--- src/main/java/com/carrepairshop/springframework/agh/security/config/SecurityConfig.java
+++ src/main/java/com/carrepairshop/springframework/agh/security/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.carrepairshop.springframework.agh.security.config;
 
+import com.carrepairshop.springframework.agh.employees.AbstractEmployee;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,27 +16,71 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final Logger log = LogManager.getLogger(SecurityConfig.class);
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin"))
-                .authorities("MANAGER");
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        class UsersRowMapper implements RowMapper<AbstractEmployee> {
+            @Override
+            public AbstractEmployee mapRow(ResultSet resultSet, int i) throws SQLException {
+                AbstractEmployee abstractEmployee = new AbstractEmployee();
+                abstractEmployee.setLogin(resultSet.getString("login"));
+                abstractEmployee.setPassword(resultSet.getString("password"));
+                abstractEmployee.setSetRole(resultSet.getString("setRole"));
+                return abstractEmployee;
+            }
+        }
+
+        log.info("searching users in table user");
+        final String sql = "SELECT login, password, setRole FROM USERS " +
+                "JOIN ROLE ON USERS.setRole=ROLE.role";
+        List<AbstractEmployee> abstractEmployees = new ArrayList<>
+                (jdbcTemplate.query(sql, new UsersRowMapper()));
+
+        for (AbstractEmployee a : abstractEmployees) {
+            String login = a.getLogin();
+            String password = a.getPassword();
+            String role = a.getSetRole().toUpperCase();
+
+            auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+                    .withUser(login).password(passwordEncoder.encode(password)).roles(role);
+        }
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin().and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers( "/").permitAll()
-                //;
-                .anyRequest().authenticated();    //TOGGLE FOR DISABLE/ENABLE BASIC AUTH;
+        /**
+         * Set security true/false for disable/enable security authentication
+         * Mention that disabling security should be used only with dev/test purposes!
+         **/
+        boolean security = false;
+        if (!security)
+            http.authorizeRequests().antMatchers("/").permitAll();
+        else {
+            http
+                    .formLogin().and()
+                    .csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/api/**").hasAnyRole("MANAGER", "MECHANIC", "ACCOUNTANT", "LOGISTICIAN")
+                    .anyRequest().authenticated();
+        }
     }
 
     @Bean
