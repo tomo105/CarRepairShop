bd1eb2e999d63b72e4af96fd0f0b6720db5e0de6
Lukasz Kuk <lukasz0104201@wp.pl>
2020-05-01 01:51:57 +0200

    calendar prototype

---
+++ src/main/java/com/carrepairshop/springframework/agh/employees/Logistician.java
@@ -0,0 +1,21 @@
+package com.carrepairshop.springframework.agh.employees;
+
+import com.carrepairshop.springframework.agh.calendars.WorkCalendar;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Logistician extends AbstractEmployee {
+
+    private WorkCalendar<Logistician> workCalendar;
+
+    @Builder
+    public Logistician(Integer id, Integer idRole, String role, String name, String surname,
+                       String login, String password, Integer experience, Integer experienceInCompany,
+                       WorkCalendar<Logistician> workCalendar) {
+        super(id, idRole, role, name, surname, login, password, experience, experienceInCompany);
+        this.workCalendar = workCalendar;
+    }
+}
---
+++ src/main/java/com/carrepairshop/springframework/agh/employees/Mechanic.java
@@ -0,0 +1,21 @@
+package com.carrepairshop.springframework.agh.employees;
+
+import com.carrepairshop.springframework.agh.calendars.WorkCalendar;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mechanic extends AbstractEmployee {
+
+    private WorkCalendar<Mechanic> workCalendar;
+
+    @Builder
+    public Mechanic(Integer id, Integer idRole, String role, String name, String surname,
+                    String login, String password, Integer experience, Integer experienceInCompany,
+                    WorkCalendar<Mechanic> workCalendar) {
+        super(id, idRole, role, name, surname, login, password, experience, experienceInCompany);
+        this.workCalendar = workCalendar;
+    }
+}
