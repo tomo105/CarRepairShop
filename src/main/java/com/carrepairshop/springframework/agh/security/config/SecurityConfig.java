c4910f27cd15ed65182f8ad3b5af543a55db86b0
Franciszek Czana <franciszek.czana@futurum.tech>
2020-05-16 16:08:38 +0200

    add referer for redirecting after successful login

--- src/main/java/com/carrepairshop/springframework/agh/security/config/SecurityConfig.java
+++ src/main/java/com/carrepairshop/springframework/agh/security/config/SecurityConfig.java
@@ -20,7 +20,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -86,6 +91,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/**").hasAnyRole("MANAGER", "MECHANIC", "ACCOUNTANT", "LOGISTICIAN")
                     .anyRequest().authenticated();
         }
+    }
+
+    @Bean
+    public RequestCache refererRequestCache() {
+        return new HttpSessionRequestCache() {
+            @Override
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                String referrer = request.getHeader("referer");
+                if (referrer != null) {
+                    request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST", new SimpleSavedRequest(referrer));
+                }
+            }
+        };
     }
 
     @Bean
